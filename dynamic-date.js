76b149f0a031e1ce7a42992f7ecd472214ff277a
alando720 <alan.do.720@gmail.com>
2026-03-25 15:24:41 -0500

    Update dynamic-date.js

--- dynamic-date.js
+++ dynamic-date.js
@@ -47,7 +47,7 @@ function validateSSN() {
         return true;
     }
 }
-
+//address 1 validation code
 function validateAddress1() {
     var addr1 = document.getElementByID("address1").value;
     console.log(addr1);
@@ -63,7 +63,39 @@ function validateAddress1() {
     }
 }  
 
+//zip code validation code
+function validatezip() {
+    const zipInput = document.getElementByID("zip");
+    let zip = zipInput.value.replace(/[^\d-]/g, "") //takes out any thing thats not a number or dash
 
+  if (!zip) {
+      document.getElementByID("zipcode-error").innerHTML = "Zip code cannot be blank."
+      return false;
+  } 
+  if (zip.length > 5){
+      zip = zip.slice(0,5); //Won't let zip be larger than 5
+  }
+
+  zipInput.value = zip;
+  document.getElementByID("zipcode-error").innerHTML = "";
+  return true;
+}
+//email validation code
+function validateEmail() {
+    email = document.getElementByID("email").value;
+    var emailR = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.w{2,3})+$/; //regular expression pattern for email
+
+    if (email =="") {
+        document.getElementByID("email-error").innerHTML = "Email cannot be empty.";
+        return false;
+    }  else if (!email.match(emailR) {
+          document.getElementByID("email-error").innerHTMl = "Enter a valid email."
+          return false;
+    }  else {
+          document.getElementByID("email-erorr").innerHTML = "";
+          return true;
+    }
+}
 
 
 
